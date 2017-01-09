6eeacc831bf9cfe911d1c3028439e096e3cf655d
Nolan Stover <nstover@fusionbox.com>
2017-01-09 15:29:58 -0700

    Run green light on every loop + ...DISCO PARTY TIME!
    Fixes bug where diodes were left on in faulty states.
    Disco party time is an easter egg. Dancing required.

--- plight.ino
+++ plight.ino
@@ -6,14 +6,16 @@
 #define CLOCK_PIN_A 3
 #define SENSOR_DATA_PIN 5
 
-
 #define STRIP_LENGTH 72
 #define ONE_LIGHT (STRIP_LENGTH/3)
 
 #define ALL_OFF 0x000000
-#define RED 0x00ff00
-#define YELLOW 0x507f00 
-#define GREEN 0xff0000
+#define RED strip.Color(32, 0, 0)
+#define YELLOW strip.Color(24, 10, 0)
+#define GREEN strip.Color(0, 16, 0)
+
+#define DISCO 69
+#define DISCO_FREQ 250
 
 LPD8806 strip(STRIP_LENGTH, DATA_PIN_A, CLOCK_PIN_A);
 
@@ -28,7 +30,6 @@ void setup() {
   green_light();
 }
 
-
 void clear() {
   int i;
   for ( i = 0; i < STRIP_LENGTH; i++ )
@@ -36,12 +37,54 @@ void clear() {
   strip.show();
 }
 
+uint32_t Wheel(uint16_t WheelPos)
+{
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128;   //red down
+      g = WheelPos % 128;      //green up
+      b = 0;                  //blue off
+      break;
+    case 1:
+      g = 127 - WheelPos % 128;  //green down
+      b = WheelPos % 128;      //blue up
+      r = 0;                  //red off
+      break;
+    case 2:
+      b = 127 - WheelPos % 128;  //blue down
+      r = WheelPos % 128;      //red up
+      g = 0;                  //green off
+      break;
+  }
+  return(strip.Color(r,g,b));
+}
+
+void party_time(uint8_t wait)
+{
+  for (int j=0; j < (384 * 6); j+=16) {     // cycle all 384 colors in the wheel six times
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 384));    //turn every third pixel on
+        }
+        strip.show();
+
+        delay(wait);
+
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
 void set_light_color(int light, uint32_t color)
 {
   int i;
   for ( i = ONE_LIGHT * light; i < ONE_LIGHT * (light + 1); i++ )
     strip.setPixelColor(i, color);
-} 
+}
 
 void red_light()
 {
@@ -113,18 +156,36 @@ void loop() {
   }
   else if ( current_state == RED )
   {
-    yellow_started = millis();
-    yellow_light();
-    current_state = YELLOW;
+    if ( random(0, DISCO_FREQ) == 0 )
+    {
+      current_state = DISCO;
+    }
+    else
+    {
+      yellow_started = millis();
+      yellow_light();
+      current_state = YELLOW;
+    }
+  }
+  else if ( current_state == DISCO )
+  {
+    party_time(50);
+    current_state = GREEN;
   }
   else if ( current_state == YELLOW )
   {
     // the second clause of the `||` is to account for rollover
-    if ( millis() - yellow_started >= 2500 || millis() < yellow_started )
+    if ( millis() - yellow_started >= 30000 || millis() < yellow_started )
     {
       yellow_started = 0;
-      green_light();
       current_state = GREEN;
     }
+    else {
+      yellow_light();
+    }
+  }
+  else
+  {
+    green_light();
   }
 }
